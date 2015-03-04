b03db53359ff7401171f246b0b9b47ae2414f030
Jiin <neri.barnini@gmail.com>
2015-03-04 13:17:57 +0100

    added range-filter directive

---
+++ filters/range-filter.js
@@ -0,0 +1,14 @@
+// Usage: <img ng-repeat="n in 5 | range"> or <img ng-repeat="n in myVar | range">
+
+angular.module('rangeFilter', [])
+  .filter('range', function() {
+    return function(total) {
+      var to = parseInt(total);
+      var iterations = [];
+
+      for(var i = 0; i < to; i++)
+        iterations.push(i);
+        
+      return iterations;
+    }
+  })
